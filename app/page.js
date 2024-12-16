a0827786897be011db77cbacd20a4ccc3c4ea86e
Telly-McGuire <ryandiovenaguilar@gmail.com>
2024-12-16 23:25:35 +0800

    added stuff

--- app/page.js
+++ app/page.js
@@ -32,14 +32,57 @@ export default function Home() {
 
       <main className={styles.main}>
 
-        <h1 style={{textAlign:"center"}}>Welcome to Republic Of the Philippines</h1>
-        <h2 style={{textAlign:"center"}}> Public Feedback Gathering System</h2>
-        <div className={styles.center}>
+        <div className={styles.scrollable}>
+        <h1>Welcome to Republic Of the Philippines</h1>
+        <h2> Public Feedback Gathering System</h2>
+
+        <h2>ARTICLE III</h2>
+        <h3>BILL OF RIGHTS</h3>
+        <p>There are several important laws and provisions within the 
+          Constitution that are critical to maintaining order, 
+          protecting rights, and ensuring that the government operates in a fair and just manner. 
+          These laws serve as the foundation for governance and provide safeguards for the well-being and rights of citizens.</p>
+
+        <ul>
+
+          <li>Section 4
+            <p>
+            No law shall be passed abridging the freedom of speech, of expression, 
+            or of the press, or the right of the people peaceably to assemble and petition the government for redress of grievances.
+            </p>
+          </li>
+
+          <li>Section 7
+            <p>
+            The right of the people to information on matters of public concern shall be recognized.
+            Access to official records, and to documents and papers pertaining to official acts, transactions, or decisions, 
+            as well as to government research data used as basis for policy development, shall be afforded the citizen, 
+            subject to such limitations as may be provided by law.
+            </p>
+          </li>
+
+          <li>Section 11.
+            <p>
+            Free access to the courts and quasi-judicial bodies
+            and adequate legal assistance shall not be denied to any person by reason of poverty.
+            </p>
+          </li>
+
+          <li>Section 15.
+            <p>
+            The privilege of the writ of habeas corpus shall not 
+            be suspended except in cases of invasion or rebellion, when the public safety requires it.
+            </p>
+          </li>
+
+        </ul>
+
           <button className={styles.button}>
             <a href="#Link">
               <span>Enter Feedback Here</span>
             </a>
           </button>
+
         </div>
       </main>
 
